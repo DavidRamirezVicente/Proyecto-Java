e76559b6897f68975ea7bbdf141db42c46e0e77a
Hector Gomez <hectorgomez2312@gmail.com>
2022-09-09 15:08:54 +0200

    Login funcional y copia api

--- Hector/js/preguntas.js
+++ Hector/js/preguntas.js
@@ -1,19 +1,33 @@
-fetch('http://localhost:8080/all')
-.then(res => res.json())
-.then(data => {
+document.getElementById("preguntas").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const categoria = document.getElementById("categoria").value;
+    console.log(categoria);
+    const pregunta = document.getElementById("pregunta").value;
+    console.log(pregunta);
 
-    // console.log(data);
-    let str = '';
-    data.map(item => {
-        str += `
-            
-
-                <p>${item.preguntas}</p>
-              
-        `
-    });
-
-    document.getElementById('preguntas').innerHTML = str;
-
-
-});
+    preguntas(categoria, pregunta);
+  
+  });
+  
+  function preguntas(categoria, pregunta) {
+      const url = 'http://localhost:8080/questions';
+      const params = {
+          headers: {
+              'Accept': "application/json, text/plain, */*",
+              'Content-Type': "application/json;charset=utf-8"
+          },
+          body: JSON.stringify({
+              "categoria": categoria,
+              "pregunta": pregunta
+          }),
+          method: "POST"
+      };
+  
+      alert(1);
+  fetch(url, params)
+      .then(data => { console.log('data', data)})
+      .then(res => { console.log('res', res) })
+      .catch(error => { console.log('error', error) });
+   
+  }
+  
--- Hector/js/registro.js
+++ Hector/js/registro.js
@@ -1,26 +1,26 @@
 document.getElementById("formulario").addEventListener("submit", (e) => {
     e.preventDefault();
-    const email = document.getElementById("email").value;
-    console.log(email);
-    const nombre = document.getElementById("nombre").value;
-    console.log(nombre);
-    const password = document.getElementById("password").value;
-    console.log(password);
-    registro(email, nombre, password);
+    const Email = document.getElementById("Email").value;
+    console.log(Email);
+    const UserName = document.getElementById("UserName").value;
+    console.log(UserName);
+    const Passwd = document.getElementById("Passwd").value;
+    console.log(Passwd);
+    registro(Email, UserName, Passwd);
   
   });
   
-  function registro(email, nombre, password) {
-      const url = 'http://localhost:8080/all';
+  function registro(Email, UserName, Passwd) {
+      const url = 'http://localhost:8080/users';
       const params = {
           headers: {
               'Accept': "application/json, text/plain, */*",
               'Content-Type': "application/json;charset=utf-8"
           },
           body: JSON.stringify({
-              "nombre": nombre,
-              "email": email,
-              "password": password
+              "UserName": UserName,
+              "Email": Email,
+              "Passwd": Passwd
           }),
           method: "POST"
       };
