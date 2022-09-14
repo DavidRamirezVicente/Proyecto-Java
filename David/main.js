61b9577d3135b3ef1ce843a31e24d19414b647e4
DavidRamirezVicente <ramirezvicentedavid@gmail.com>
2022-09-14 15:59:01 +0200

    detail html

--- David/main.js
+++ David/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //NavBar
 function hideIconBar(){
     var iconBar = document.getElementById("iconBar");
@@ -25,31 +24,3 @@ function showReply(){
     var replyArea = document.getElementById("reply-area");
     replyArea.classList.remove("hide");
 }
-=======
-//NavBar
-function hideIconBar(){
-    var iconBar = document.getElementById("iconBar");
-    var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:none;");
-    navigation.classList.remove("hide");
-}
-
-function showIconBar(){
-    var iconBar = document.getElementById("iconBar");
-    var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:block;");
-    navigation.classList.add("hide");
-}
-
-//Comment
-function showComment(){
-    var commentArea = document.getElementById("comment-area");
-    commentArea.classList.remove("hide");
-}
-
-//Reply
-function showReply(){
-    var replyArea = document.getElementById("reply-area");
-    replyArea.classList.remove("hide");
-}
->>>>>>> dc67eeca1adea9c5584a3a51f32d1ec576033f25
